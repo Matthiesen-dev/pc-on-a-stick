2b4f9119ad1695c3ece0bcb84ec061d9fc58d4bc
Adam Matthiesen <amatthiesen@outlook.com>
2026-03-30 15:49:47 -0700

    Move to use Cobblemon's exported utilities for the openPC method

--- src/main/java/xyz/station48/pc_on_a_stick/items/PcOnAStickItem.java
+++ src/main/java/xyz/station48/pc_on_a_stick/items/PcOnAStickItem.java
@@ -3,13 +3,17 @@ package xyz.station48.pc_on_a_stick.items;
 import com.cobblemon.mod.common.Cobblemon;
 import com.cobblemon.mod.common.CobblemonSounds;
 import com.cobblemon.mod.common.api.storage.pc.PCStore;
+import com.cobblemon.mod.common.api.storage.pc.link.PCLink;
 import com.cobblemon.mod.common.api.storage.pc.link.PCLinkManager;
-import com.cobblemon.mod.common.battles.BattleRegistry;
 import com.cobblemon.mod.common.net.messages.client.storage.pc.OpenPCPacket;
+import com.cobblemon.mod.common.util.PlayerExtensionsKt;
 import com.ofekn.crafting_on_a_stick.api.IWheelItem;
 import com.ofekn.crafting_on_a_stick.api.Ref;
+import net.minecraft.ChatFormatting;
 import net.minecraft.MethodsReturnNonnullByDefault;
+import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
@@ -26,22 +30,26 @@ public class PcOnAStickItem extends Item implements IWheelItem {
     }
 
     public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand usedHand) {
-        openPC(player);
+        openPCStorage(player);
         return InteractionResultHolder.sidedSuccess(player.getItemInHand(usedHand), level.isClientSide);
     }
 
     public void onWheelAction(Player player, Ref<ItemStack> ref) {
-        openPC(player);
+        openPCStorage(player);
     }
 
-    public static void openPC(Player player) {
-        player.playSound(CobblemonSounds.PC_ON, 0.5F, 1.0F);
+    public static void openPCStorage(Player player) {
         if (player instanceof ServerPlayer serverPlayer) {
-             if (BattleRegistry.getBattleByParticipatingPlayer(serverPlayer) == null) {
-                 PCStore pc = Cobblemon.INSTANCE.getStorage().getPC(serverPlayer);
-                 PCLinkManager.INSTANCE.addLink(player.getUUID(), pc, (p) -> true);
-                 (new OpenPCPacket(pc, 0)).sendToPlayer(serverPlayer);
-             }
+            // Check if player is in Battle
+            if(PlayerExtensionsKt.isInBattle(serverPlayer)) {
+                player.sendSystemMessage(Component.translatable("cobblemon.pc.inbattle").withStyle(ChatFormatting.RED));
+                return;
+            }
+            // Not in a Battle? Open PC
+            PCStore pc = Cobblemon.INSTANCE.getStorage().getPC(serverPlayer);
+            PCLinkManager.INSTANCE.addLink(new PCLink(pc, player.getUUID()));
+            player.level().playSound(null, player.blockPosition(), CobblemonSounds.PC_ON, SoundSource.NEUTRAL, 0.5f, 1.0f);
+            new OpenPCPacket(pc).sendToPlayer(serverPlayer);
         }
     }
 }
